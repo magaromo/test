05b034ce870707d28462d37dea80873befad67a3
magaromo <147823011+magaromo@users.noreply.github.com>
2025-08-17 17:21:07 +0200

    Removed function added print

--- test.py
+++ test.py
@@ -1,16 +1,4 @@
 print("hello")
 
 
-# Example of a function that checks the gate mode and returns the corresponding path
-def get_path_for_gate_mode(self, response):
-    if self.gate_mode not in self.modes:
-        self.get_logger().error(f"Invalid gate mode: {self.gate_mode}")
-        response.success = False
-        response.message = f"Invalid gate mode: {self.gate_mode}"
-        return response
-
-    self.get_logger().info(f"Valid gate mode: {self.gate_mode}")
-    index = self.modes.index(self.gate_mode)
-    the_path = self.function_list[index]()
-    
-    return the_path
+print("hlsdlsdsdsd")
